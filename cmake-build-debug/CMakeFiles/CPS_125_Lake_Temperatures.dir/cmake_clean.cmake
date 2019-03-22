995cc8774c2dc38a75eebd99572dce98427a6d4d
Karthik Gurram <kggurram@gmail.com>
2019-03-21 22:26:52 -0400

    1 - Created CLion Project
    Created project in IDE to begin.

---
+++ cmake-build-debug/CMakeFiles/CPS_125_Lake_Temperatures.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CPS_125_Lake_Temperatures.dir/main.c.obj"
+  "CPS_125_Lake_Temperatures.pdb"
+  "CPS_125_Lake_Temperatures.exe"
+  "CPS_125_Lake_Temperatures.exe.manifest"
+  "libCPS_125_Lake_Temperatures.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/CPS_125_Lake_Temperatures.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/CPS_125_Lake_Temperatures.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for CPS_125_Lake_Temperatures.
+# This may be replaced when dependencies are built.
