b45cdcf88e3f7bd0c94875bdd40c34b6df1ee26b
Uzair <uzairmahmed@gmail.com>
2019-03-30 19:34:12 -0400

    2.2 Tested CLion
    Basically tried to work with CLion and seeing if data collection works nicely and now it does, but the data files need too be stored in the debug folder otherwise it won't work.

--- main.c
+++ main.c
@@ -1,6 +1,23 @@
 #include <stdio.h>
 
+void GetData() {
+    FILE *file;
+    file = fopen("LakeTemp2017.dat", "r");
+    for (int i = 0; i < 365; i++){
+        for (int j = 0; j < 8; j++){
+            double n;
+            fscanf(file, "%lf", &n);
+
+            if (j == 0) printf("%4d ", (int)n);
+            else if (j==1) printf("%3d ", (int)n);
+            else printf("%lf ", n);
+        }
+        printf("\n");
+    }
+    fclose(file);
+}
+
 int main() {
-    printf("Hello, World!\n");
+    GetData();
     return 0;
 }
