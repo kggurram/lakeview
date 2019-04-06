b04e42656f1482b46fbe7714a8200719868fa119
Uzair <uzairmahmed@gmail.com>
2019-04-06 02:43:23 -0400

    Question 3 Done (ish)
    Values are off.

--- main.c
+++ main.c
@@ -40,21 +40,56 @@ void avgAnalysis(char *nameData[6], double averageData[6]){
     printf("\t\t\t--------\n");
 }
 /* Part 3 - Uzair*/
-void perDayHotnCold(char *nameData[6], double data[]){}
+void convertToDDMM(int day, int *MM, int *DD){
+    /*Store the length of each month*/
+    int mday[] = {31,28,31,30,31,30,31,31,30,31,30,31};
+    int months = 12;
+    int sum = 0;
+    for(int i = 1; i <= months; i++){
+        /*if there are days left to account for, account for them*/
+        if (day >= sum && day <= sum+mday[i] && i<months){
+            *MM = i;
+            *DD = day-sum;
+            break;
+        }
+        /*if its december, account for the last few days*/
+        else if (i ==months){
+            *MM = months;
+            *DD = day-sum;
+            break;
+        }
+        sum += mday[i-1];
+    }
+}
+void perDayHotnCold(char *nameData[6], double data[365][8], double coldestDays[6], double hottestDays[6], char *monthName[12]){
+    int coldest = 0, hottest = 0;
+
+    printf("Lake\t\tWarmest\t  Date\t\tColdest\t  Date\n");
+
+    for(int i = 0; i < 6; i++){
+        int MM=0, MM2=0, DD=0, DD2=0;
+        for(int j = 0; j<365; j++){
+            if(data[coldest][i+2] > data[j][i+2]) coldest = j;
+            else if(data[hottest][i+2] < data[j][i+2]) hottest = j;
+        }
+        coldestDays[i]=coldest;
+        hottestDays[i]=hottest;
+
+        convertToDDMM(hottest, &MM, &DD);
+        convertToDDMM(coldest, &MM2, &DD2);
+
+        printf("Lake %s:\t%5.2lf  \t%s %d\t\t%5.2lf  \t%s %d\n", nameData[i], data[hottest][i+2], monthName[MM], DD, data[coldest][i+2], monthName[MM2], DD2);
+    }
+}
 
 int main() {
     /* Initialize 2D Array for name Strings */
-    char *nameData[6];
-    nameData[0] = "Superior";
-    nameData[1] = "Michigan";
-    nameData[2] = "Huron";
-    nameData[3] = "Erie";
-    nameData[4] = "Ontario";
-    nameData[5] = "St. Clair";
+    char *nameData[6] = {"Superior", "Michigan", "Huron", "Erie","Ontario","St. Clair"};
+    char *monthName[12] = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
     /* Initialize 2D Array for data*/
     double data[365][8];
     /* Initialize data for every question*/
-    double averageData[6] = {0};
+    double averageData[6] = {0}, coldestDays[6] = {0}, hottestDays[6] = {0};
 
     /* Initialize Read-Only File */
     FILE *file;
@@ -71,6 +106,7 @@ int main() {
 
     averages(nameData, data, averageData);
     avgAnalysis(nameData, averageData);
+    perDayHotnCold(nameData, data, coldestDays, hottestDays, monthName);
 
     return 0;
 }
